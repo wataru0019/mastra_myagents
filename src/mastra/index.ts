ae4ff5d211e64a140dcc2ea0e97faddd97022cc7
WataruKashihara <kashiwabarawataru@kashiwabarawatarunoMacBook-Pro.local>
2025-08-05 23:25:53 +0900

    vercel

--- src/mastra/index.ts
+++ src/mastra/index.ts
@@ -1,5 +1,6 @@
 globalThis.___MASTRA_TELEMETRY___ = true;
 import { Mastra } from "@mastra/core";
+import { VercelDeployer } from "@mastra/deployer-vercel";
 
 import { openAiAgent } from "./agents/openai_agent";
 
@@ -7,6 +8,7 @@ export const mastra = new Mastra({
     agents: {
         openAiAgent: openAiAgent
     },
+    deployer: new VercelDeployer()
 })
 
 export async function run() {
